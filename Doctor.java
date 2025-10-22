0a19a5892565b03d75c1a34c2a7f7d27c9d0b34f
AARIF HUSSAIN RABBANI <153491898+ahrabbani@users.noreply.github.com>
2025-10-23 00:05:52 +0530

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -9,7 +9,9 @@ public class Doctor {
         this.specialization = specialization;
     }
 
-    public int getId() { return id; }
+    public int getId() { 
+        return id;
+    }
     public String getName() { return name; }
     public String getSpecialization() { return specialization; }
 
